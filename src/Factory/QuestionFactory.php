6611689ab6450ee4fb2f5f4c9b0faf929909d647
Daniel B <danielynx@proton.me>
2023-12-20 10:07:48 -0400

    migrate from old account

---
+++ src/Factory/QuestionFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @method static Question|Proxy createOne(array $attributes = [])
+ * @method static Question[]|Proxy[] createMany(int $number, $attributes = [])
+ * @method static Question|Proxy find($criteria)
+ * @method static Question|Proxy findOrCreate(array $attributes)
+ * @method static Question|Proxy first(string $sortedField = 'id')
+ * @method static Question|Proxy last(string $sortedField = 'id')
+ * @method static Question|Proxy random(array $attributes = [])
+ * @method static Question|Proxy randomOrCreate(array $attributes = [])
+ * @method static Question[]|Proxy[] all()
+ * @method static Question[]|Proxy[] findBy(array $attributes)
+ * @method static Question[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Question[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static QuestionRepository|RepositoryProxy repository()
+ * @method Question|Proxy create($attributes = [])
+ */
+final class QuestionFactory extends ModelFactory
+{
+
+    public function unpublished(): self
+    {
+        return $this->addState(['askedAt' => null]);
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            'name' => self::faker()->realText(50),
+            'question' => self::faker()->paragraphs(
+                self::faker()->numberBetween(1, 4),
+                true
+            ),
+            'askedAt' => self::faker()->dateTimeBetween('-100 days', '-1 minute'),
+            'votes' => self::faker()->numberBetween(-20, 40),
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://github.com/zenstruck/foundry#initialization
+        return $this
+            ->afterInstantiate(function(Question $question) {
+               if (!$question->getSlug()) {
+                   $slugger = new AsciiSlugger();
+                   $question->setSlug($slugger->slug($question->getName()));
+               } 
+            })
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Question::class;
+    }
+}
